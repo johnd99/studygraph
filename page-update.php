e85cb9e4e80d481fcf1a49cb3f037c96b0fe98ef
johnd99 <87199350+johnd99@users.noreply.github.com>
2023-11-14 19:46:24 -0500

    Simplified image handling logic

--- page-update.php
+++ page-update.php
@@ -6,12 +6,8 @@
     $body = $_POST['page-body'];
     $id = $_POST['id'];
 
-    $imageData = null;
     if (isset($_FILES['imageInput']) && $_FILES['imageInput']['size'] > 0) {
         $imageData = file_get_contents($_FILES['imageInput']['tmp_name']);
-    }
-
-    if ($imageData) {
         $sql = "UPDATE page SET title = ?, body = ?, image_data = ? WHERE id = ?";
         $stmt = $conn->prepare($sql);
         $stmt->execute([$title, $body, $imageData, $id]);
